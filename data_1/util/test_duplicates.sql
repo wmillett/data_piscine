495667ab6c14445efe08d8d94613c41169f6d964
wmillett <williammillette98@gmail.com>
2025-06-10 21:47:12 +0200

    added more make commands for testing and setup

---
+++ data_1/util/test_duplicates.sql
@@ -0,0 +1,52 @@
+DO $$
+DECLARE
+    count_result BIGINT;
+BEGIN
+    RAISE NOTICE 'Testing for duplicates in the customers table...';
+    RAISE NOTICE 'The following count should be of 1 if there are no duplicates:';
+
+    SELECT COUNT(*) INTO count_result
+    FROM customers
+    WHERE event_time = '2022-10-01 00:00:30'
+      AND event_type = 'remove_from_cart'
+      AND product_id = 5809103;
+    RAISE NOTICE 'Count: %', count_result;
+
+    RAISE NOTICE 'The following count should be of 1 and 0 if there are no duplicates:';
+
+    SELECT COUNT(*) INTO count_result
+    FROM customers
+    WHERE event_time = '2022-10-01 00:00:32'
+      AND event_type = 'remove_from_cart'
+      AND product_id = 5809103;
+    RAISE NOTICE 'Count: %', count_result;
+
+    SELECT COUNT(*) INTO count_result
+    FROM customers
+    WHERE event_time = '2022-10-01 00:00:33'
+      AND event_type = 'remove_from_cart'
+      AND product_id = 5809103;
+    RAISE NOTICE 'Count: %', count_result;
+
+    RAISE NOTICE 'The following count should be of 1 and 1 and not lower:';
+
+    SELECT COUNT(*) INTO count_result
+    FROM customers
+    WHERE event_time = '2022-10-01 00:04:15'
+      AND event_type = 'remove_from_cart'
+      AND product_id = 5692893;
+    RAISE NOTICE 'Count: %', count_result;
+
+    SELECT COUNT(*) INTO count_result
+    FROM customers
+    WHERE event_time = '2022-10-01 00:04:15'
+      AND event_type = 'remove_from_cart'
+      AND product_id = 5802443;
+    RAISE NOTICE 'Count: %', count_result;
+
+    RAISE NOTICE 'The following count should be between 18500000 and 19200000:';
+
+    SELECT COUNT(*) INTO count_result
+    FROM customers;
+    RAISE NOTICE 'Count: %', count_result;
+END $$;
